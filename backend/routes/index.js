838b82315f427e2a7f18e3fe099050280182e27d
Egor <egor297198@mail.ru>
2026-05-03 20:29:18 +0400

    feat(backend): добавил backend часть

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,63 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const {
+  UserController,
+  PostController,
+  CommentController,
+  LikeController,
+  FollowController,
+} = require("../controllers");
+const authenticateToken = require("../middleware/auth");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const destination = "uploads";
+
+//Показываем, место хранения файлов
+const storage = multer.diskStorage({
+  destination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+
+const uploads = multer({ storage });
+
+//Роуты для пользователя
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/current", authenticateToken, UserController.current);
+router.get("/users/:id", authenticateToken, UserController.getUserById);
+router.put(
+  "/users/:id",
+  authenticateToken,
+  uploads.single("avatar"),
+  UserController.updateUser
+);
+
+//Роуты для постов
+router.post("/posts", authenticateToken, PostController.createPost);
+router.get("/posts", authenticateToken, PostController.getAllPosts);
+router.get("/posts/:id", authenticateToken, PostController.getPostById);
+router.delete("/posts/:id", authenticateToken, PostController.deletePost);
+
+//Роуты для комментариев
+router.post("/comments", authenticateToken, CommentController.createComment);
+router.delete(
+  "/comments/:id",
+  authenticateToken,
+  CommentController.deleteComment
+);
+
+//Роту для лайков
+router.post("/likes", authenticateToken, LikeController.likePost);
+router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
+
+//Роту для подписок
+router.post("/follow", authenticateToken, FollowController.followUser);
+router.delete(
+  "/unfollow/:id",
+  authenticateToken,
+  FollowController.unfollowUser
+);
 
 module.exports = router;
