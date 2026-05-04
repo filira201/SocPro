b4a3e306458359b8cff6aeb5dc762052ff09da21
Egor <egor297198@mail.ru>
2026-05-04 09:47:05 +0400

    feat(frontend): удалил папкет для алиасов, ведб теперь vite сам эт предоставляет

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
